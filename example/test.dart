42296e55cb6442adec0fb480dc19067a158255a6
Ranggaarg <2111500035@student.budiluhur.ac.id>
2023-09-14 07:15:10 +0700

    Dart library

---
+++ example/test.dart
@@ -0,0 +1,10 @@
+import 'package:belajar_dart_packages_library/halo.dart';
+import 'package:belajar_dart_packages_library/src/customer.dart%20';
+
+void main() {
+  var sayMorning = sayHello('Rangga');
+  print(sayMorning);
+
+  var customer = Customer();
+  var product = Product();
+}
---
+++ lib/src/say_hello.dart
@@ -0,0 +1,3 @@
+String sayHello (String name) {
+  return 'Halo Selamat Pagi $name';
+}
